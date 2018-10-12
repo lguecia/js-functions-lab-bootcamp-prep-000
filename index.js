5e18dc781cf13f4feeb817833fd1ea937d26ed96
Leah Guecia <lguecia@gmail.com>
2018-10-12 03:45:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo(you){
   return ('Happy holidays, you!');
 }
 function happyHolidayTo(holiday, name){
-  return ('Happy ${holiday}, ${name}!');
+  return ('Happy Independence Day, you!');
 }
 function holidayCountdown(holiday, days){
   return("it's ${days} until ${holiday}!");
